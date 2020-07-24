50a83926bc8a826cb339b181dffbf7151f6eedfa
David Lam <david.lam@winc.com.au>
2020-07-24 13:13:27 +1000

    commit

--- product.go
+++ product.go
@@ -10,7 +10,7 @@ import (
 
 var (
 	base64Table string = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz{}"
-	indexMap      map[rune]int
+	indexMap    map[rune]int
 )
 
 func init() {
@@ -24,14 +24,18 @@ func init() {
 func C22ToHex(c22 string) string {
 	binaryChunks := make([]string, 0, 22)
 	for _, ch := range c22 {
+		// each number becomes a chunk of 6 bits.
 		binaryChunks = append(binaryChunks, fmt.Sprintf("%06b", indexMap[ch]))
 
 	}
 	bigInt := new(big.Int)
 	b := strings.Join(binaryChunks, "")
+	// UUID is 128 bits.
+	// 22 chunks of 6 bits = 22*6 = 132 bits which is longer than 128 bits by 4.  Therefore the last 4 bits are discarded.
 	b = b[:len(b)-4]
 	bigInt.SetString(b, 2)
 	h := bigInt.Text(16)
+	// padding zeros to make full 32 characters
 	h = strings.Repeat("0", 32-len(h)) + h
 	return strings.ToUpper(h)
 
@@ -43,7 +47,7 @@ func HexToC22(hex string) string {
 	bigInt := new(big.Int)
 	bigInt.SetString(hex, 16)
 	b := bigInt.Text(2)
-
+	// UUID is 128 bits. Pad zeros on left, and add 4 bits, to make 132 bits, which will become 22 chunks of 6 bits.
 	b = strings.Repeat("0", 128-len(b)) + b + "0000"
 
 	//fmt.Println(b)
