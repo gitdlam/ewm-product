5845f9f0284d87dcbb83410b8a6bb58679ed2771
David Lam <david.lam@winc.com.au>
2020-07-26 00:43:02 +1000

    Added Encode() and Decode() based on standard base64 library. Faster.

--- product.go
+++ product.go
@@ -1,8 +1,8 @@
 package product
 
 import (
+	"bytes"
 	"fmt"
-
 	"math/big"
 	"strings"
 	//"strconv"
@@ -63,4 +63,21 @@ func HexToC22(hex string) string {
 	}
 
 	return string(c22)
+}
+
+// This is added later based on the standard library code. Runs 10x faster.
+func Encode(hex string) string {
+	bigInt := new(big.Int)
+	bigInt.SetString(hex, 16)
+	return RawCustomEncoding.EncodeToString(append(bytes.Repeat([]byte{byte(0)}, 16-len(bigInt.Bytes())), bigInt.Bytes()...))
+
+}
+
+// This is added later based on the standard library code. Runs 10x faster.
+func Decode(c22 string) string {
+	b, _ := RawCustomEncoding.DecodeString(c22)
+	bigInt := new(big.Int)
+	bigInt.SetBytes(b)
+	s := strings.ToUpper(bigInt.Text(16))
+	return strings.Repeat("0", 32-len(s)) + s
 }
